89a468902e8970045f710e54e3d55e3802b3e773
ilya24037 <ilya24037@gmail.com>
2025-06-07 09:20:59 +0500

    1

--- app/Models/Animator.php
+++ app/Models/Animator.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Animator extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory,
 
     protected $fillable = [
         'user_id',
