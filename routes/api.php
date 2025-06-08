ff7156fdf0a6b2c2cd02b42e28b09406bb66e2b0
ilya24037 <ilya24037@gmail.com>
2025-06-08 13:36:25 +0500

    1

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,6 @@
-<?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryApiController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
